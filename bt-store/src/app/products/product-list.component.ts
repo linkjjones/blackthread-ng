4438edf9b271babdfedccf63d25b65acda5b89ef
Jeffrey Jones <linkjjones@gmail.com>
2022-01-31 14:08:51 -0500

    Event emitter | Input Output

--- bt-store/src/app/products/product-list.component.ts
+++ bt-store/src/app/products/product-list.component.ts
@@ -59,4 +59,8 @@ export class ProductionListComponent {
     toggleImages(): void {
         this.showImage = !this.showImage;
     }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List ' + message;
+    }
 }
--- bt-store/src/app/shared/star.component.ts
+++ bt-store/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, SimpleChanges } from "@angular/core";
+import { Component, EventEmitter, OnChanges, Input, Output, SimpleChanges } from "@angular/core";
 
 @Component({
     selector: 'pm-star',
@@ -6,10 +6,15 @@ import { Component, OnChanges, SimpleChanges } from "@angular/core";
     styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnChanges {
-    rating: number = 4;
+    @Input() rating: number = 0;
     cropWidth: number = 75;
+    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
 
     ngOnChanges(): void {
         this.cropWidth = this.rating * 75/5;
+    }
+
+    onClick(): void {
+        this.ratingClicked.emit(`The Rating ${this.rating} was clicked!`);    
     }
 }
