7c2561e784a9cb1259441f0804e6ba8d3ff4f3ae
Jeffrey Jones <linkjjones@gmail.com>
2022-02-13 14:08:59 -0500

    Modules

--- bt-store/src/app/products/product.module.ts
+++ bt-store/src/app/products/product.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ReplaceStringWithStringPipe } from '../shared/replace-string-with-string.pipe';
-import { StarComponent } from '../shared/star.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
     ReplaceStringWithStringPipe,
-    StarComponent
   ],
   imports: [
-    CommonModule
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      {
+        path: 'products/:id',
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent
+      },
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
